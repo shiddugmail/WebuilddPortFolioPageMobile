40503395c00a15b62af425b5ee51e58e6a2d3ef8
Shiddu <30692292+shiddugmail@users.noreply.github.com>
2021-06-14 14:11:45 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,188 @@
+import "./App.css";
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import X03PortfolioPageMobile from "./components/X03PortfolioPageMobile";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:path(|03-portfolio-page-mobile)">
+          <X03PortfolioPageMobile
+            ourProjects="Our Projects"
+            rectangle76="https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-76-7@2x.png"
+            text166="There are many variations of passages of Lorem Ipsum available but the majority have suffered dummy alteration in that an some form by the injected humour or randomised words which don’t look even that slightly believable. If you are Lorem Ipsums your need to be sure there isn’t anything embarrassing. There are many variations passages of Lorem as Ipsums available but the majority have suffered that alterations in that some form by injected humour or randomised words which don’t looks even slightly that believable. If you are Lorem as Ipsum  you need to be sure there isn’t anything as embarrassing. There are many variation of passages Lorem Ipsum available but the majority have suffered that a aalteration in that some form by injected humour or randomised words which don’t look even slightly the believable."
+            frame145Props={x03PortfolioPageMobileData.frame145Props}
+            frame1613Props={x03PortfolioPageMobileData.frame1613Props}
+            frame1613Props2={x03PortfolioPageMobileData.frame1613Props2}
+          />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+const group86Data = {
+    group85: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/group-85-3@2x.svg",
+};
+
+const iconMenuData = {
+    vector2: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-159@2x.svg",
+    vector3: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-159@2x.svg",
+};
+
+const frame145Data = {
+    group86Props: group86Data,
+    iconMenuProps: iconMenuData,
+};
+
+const group139Data = {
+    children: "ALL",
+};
+
+const frame138Data = {
+    place: "INTERIOR",
+    living: "LIVING",
+};
+
+const projectCard22Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-24@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard222Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-25@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard223Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-26@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard224Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-27@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard225Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-28@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard226Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-29@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard227Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-30@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard228Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-31@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const projectCard229Data = {
+    rectangle62: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-62-32@2x.png",
+    text77: "Project Name",
+    surname: "Small description about the project",
+};
+
+const button22Data = {
+    children: "VIEW MORE",
+};
+
+const frame1613Data = {
+    apartments: "APARTMENTS",
+    buildiings: "BUILDIINGS",
+    group139Props: group139Data,
+    frame138Props: frame138Data,
+    projectCard22Props: projectCard22Data,
+    projectCard222Props: projectCard222Data,
+    projectCard223Props: projectCard223Data,
+    projectCard224Props: projectCard224Data,
+    projectCard225Props: projectCard225Data,
+    projectCard226Props: projectCard226Data,
+    projectCard227Props: projectCard227Data,
+    projectCard228Props: projectCard228Data,
+    projectCard229Props: projectCard229Data,
+    button22Props: button22Data,
+};
+
+const group8622Data = {
+    group85: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/group-85-26@2x.svg",
+};
+
+const facebookAppSymbolData = {
+    src: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/f-1-1@2x.svg",
+};
+
+const twitterData = {
+    src: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-157@2x.svg",
+};
+
+const group872Data = {
+    overlapGroup: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-154@2x.svg",
+    vector2: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-156@2x.svg",
+    facebookAppSymbolProps: facebookAppSymbolData,
+    twitterProps: twitterData,
+};
+
+const group322Data = {
+    place: "Contact",
+    overlapGroup: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-243@2x.svg",
+    phone: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-245@2x.svg",
+    overlapGroup1: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-246@2x.svg",
+    vector2: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-153@2x.svg",
+    text165: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
+    text164: "+91 8976535221, +91 8755645342",
+    contactWebuildCom: "contact@webuild.com",
+};
+
+const frame140Data = {
+    aboutUs: "About Us",
+    projects: "Projects",
+};
+
+const frame1402Data = {
+    aboutUs: "Gallery",
+    projects: "Knowledge Transfer",
+};
+
+const frame1403Data = {
+    aboutUs: "Blogs",
+    projects: "FAQ’s",
+};
+
+const frame5Data = {
+    quickLinks: "Quick Links",
+    frame140Props: frame140Data,
+    frame1402Props: frame1402Data,
+    frame1403Props: frame1403Data,
+};
+
+const group2122Data = {
+    group862Props: group8622Data,
+    group872Props: group872Data,
+    group322Props: group322Data,
+    frame5Props: frame5Data,
+};
+
+const x03PortfolioPageMobileData = {
+    frame145Props: frame145Data,
+    frame1613Props: frame1613Data,
+    frame1613Props2: group2122Data,
+};
+
